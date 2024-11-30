b55226ee89b2996b20c9486debe28915c7e1614a
Anastasiia.Aheienko.PM.2022@lpnu.ua <Anastasiia.Aheienko.PM.2022@lpnu.ua>
2024-11-30 12:03:59 +0200

    Add project files.

---
+++ UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using AnalaizerClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Reflection;
+
+namespace AnalizerClassLibraryTest
+{
+    [TestClass]
+    public class AnalizerClassLibraryTestClass
+    {
+        public TestContext TestContext { get; set; }
+
+       
+        [DataSource("System.Data.SqlClient", @"Data Source=(localdb)\mssqllocaldb;AttachDbFilename=C:\Users\Lenovo\Tests.mdf;Integrated Security=True;Connect Timeout=30", "TestCheckCurrency", DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void CheckCurrency_ExpressionValidity()
+        {
+            // Arrange
+            string expression = TestContext.DataRow["expression"].ToString();
+            bool expectedCheckCurrency = bool.Parse(TestContext.DataRow["CheckCurrency"].ToString());
+
+            AnalaizerClass.expression = expression;
+
+            // Act
+            bool CheckCurrency = AnalaizerClass.CheckCurrency();
+
+            // Assert
+            Assert.AreEqual(expectedCheckCurrency, CheckCurrency);
+        }
+
+       
+    }
+
+
+}
+
---
+++ UnitTestProjectTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProjectTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProjectTests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("81959462-7c4d-4c5a-8009-4d24f5787cb8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
